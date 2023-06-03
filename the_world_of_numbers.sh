7a813fbfdf3ca6246817194ecfb7c8b24c4ae2d1
IRON-NEHM <132246535+IRON-NEHM@users.noreply.github.com>
2023-06-03 12:25:00 +0200

    Create the_world_of_numbers.sh

---
+++ the_world_of_numbers.sh
@@ -0,0 +1,11 @@
+# Given two integers, X and Y, find their sum, difference, product and quotient.
+# written and codded by IRON-NEHM
+
+read x
+read y 
+
+echo "$((x + y))"      
+echo "$((x - y))"      
+echo "$((x * y))"      
+echo "$((x / y))"      
+      
